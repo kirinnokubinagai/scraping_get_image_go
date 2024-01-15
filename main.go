deb9d452c0fd3401f54391f6c216b7bec033a188
柴田大介 <kirin@shibatadaisukenoMacBook-Pro.local>
2024-01-15 14:04:03 +0900

    サイト内の全てのページをスクレイピングするように修正

--- main.go
+++ main.go
@@ -4,33 +4,53 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+var visited = make(map[string]bool)
+
 func main() {
-	// コマンドライン引数からサイトのURLを取得
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <siteURL>")
 		return
 	}
-	url := os.Args[1]
+	startURL := os.Args[1]
 
-	// サイトのHTMLを取得
-	res, err := http.Get(url)
+	crawlSite(startURL)
+}
+
+func crawlSite(urlStr string) {
+	// 既に訪れたURLは再帰的に処理しない
+	if visited[urlStr] {
+		return
+	}
+
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return
+	}
+
+	res, err := http.Get(urlStr)
 	if err != nil {
 		fmt.Println("Error Not Found URL:", err)
 		return
 	}
 	defer res.Body.Close()
 
+	// サイトのHTMLを取得
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		fmt.Println("Error fetching the URL:", err)
 		return
 	}
+
+	// このページでやりたい処理をここに追加
+	fmt.Println("Visited:", urlStr)
 
 	// 画像をダウンロードするためのディレクトリ
 	downloadDir := "./downloaded_images/"
@@ -48,7 +68,7 @@ func main() {
 		imgSrc, exists := s.Attr("src")
 		if exists {
 			// 画像のフルパスを取得
-			imgURL := getFullURL(url, imgSrc)
+			imgURL := getAbsoluteURL(u, imgSrc)
 
 			// 画像をダウンロード
 			err := downloadImage(imgURL, downloadDir)
@@ -57,20 +77,42 @@ func main() {
 			}
 		}
 	})
+
+	// このページを訪れたことを記録
+	visited[urlStr] = true
+
+	// ページ内のリンクを再帰的に処理
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		link, exists := s.Attr("href")
+		if exists {
+			// リンクの正規化
+			absURL := getAbsoluteURL(u, link)
+
+			// 同じサイトのページかどうか確認
+			if isSameSite(u, absURL) {
+				crawlSite(absURL)
+			}
+		}
+	})
 }
 
-// 相対パスの画像URLをフルパスに変換
-func getFullURL(baseURL, imgURL string) string {
-	if strings.HasPrefix(imgURL, "http") {
-		return imgURL
+// 相対パスや不正な形式のURLを絶対URLに変換
+func getAbsoluteURL(base *url.URL, href string) string {
+	relURL, err := url.Parse(href)
+	if err != nil {
+		return ""
 	}
-	if strings.HasPrefix(imgURL, "//") {
-		return "https:" + imgURL
+	absURL := base.ResolveReference(relURL)
+	return absURL.String()
+}
+
+// 同じサイトかどうかを確認
+func isSameSite(base *url.URL, target string) bool {
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		return false
 	}
-	if strings.HasPrefix(imgURL, "/") {
-		return baseURL + imgURL
-	}
-	return baseURL + "/" + imgURL
+	return base.Host == targetURL.Host
 }
 
 // 画像をダウンロード
