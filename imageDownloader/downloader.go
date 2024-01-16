95fea4830e98d1e33f68554c04cff142c42211ee
柴田大介 <kirin@shibatadaisukenoMacBook-Pro.local>
2024-01-16 14:50:26 +0900

    画像のサイズ毎にディレクトリを分ける

--- imageDownloader/downloader.go
+++ imageDownloader/downloader.go
@@ -9,17 +9,31 @@ import (
 )
 
 // DownloadImage downloads an image from the given URL to the specified directory
+// and categorizes the image based on its size.
 func DownloadImage(url, destDir string) error {
 	tokens := strings.Split(url, "/")
 	imageName := tokens[len(tokens)-1]
-
-	filePath := destDir + imageName
 
 	response, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer response.Body.Close()
+
+	// Get the image size category based on Content-Length
+	imageSize := getImageSize(response.ContentLength)
+
+	// Choose the subdirectory based on the image size
+	subDir := getSubdirectory(imageSize)
+	subDirPath := destDir + subDir + "/"
+
+	// Create the subdirectory if it doesn't exist
+	err = os.MkdirAll(subDirPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	filePath := subDirPath + imageName
 
 	file, err := os.Create(filePath)
 	if err != nil {
@@ -32,6 +46,26 @@ func DownloadImage(url, destDir string) error {
 		return err
 	}
 
-	fmt.Println("Downloaded:", imageName)
+	fmt.Printf("Downloaded %s to %s\n", imageName, subDir)
 	return nil
+}
+
+// getImageSize returns a size category based on the image size
+func getImageSize(contentLength int64) string {
+	// This is a simplified example; you may need to adjust the thresholds based on your needs
+	const smallSize = 1024 * 100  // 100 KB
+	const mediumSize = 1024 * 500 // 500 KB
+
+	if contentLength <= smallSize {
+		return "small"
+	} else if contentLength <= mediumSize {
+		return "medium"
+	} else {
+		return "large"
+	}
+}
+
+// getSubdirectory returns the subdirectory based on the image size
+func getSubdirectory(imageSize string) string {
+	return imageSize
 }
